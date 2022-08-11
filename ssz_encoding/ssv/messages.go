ca25a933b08da93ae5a3e2859a9bcae25d83d655
Alon Muroch <alonmuroch@gmail.com>
2022-08-11 15:42:40 +0300

    align partial sig structs

--- ssz_encoding/ssv/messages.go
+++ ssz_encoding/ssv/messages.go
@@ -6,6 +6,19 @@ import (
 
 //go:generate go run .../fastssz/sszgen --path . --include ../qbft,../types
 
+type PartialSigMsgType uint64
+
+const (
+	// PostConsensusPartialSig is a partial signature over a decided duty (attestation data, block, etc)
+	PostConsensusPartialSig PartialSigMsgType = iota
+	// RandaoPartialSig is a partial signature over randao reveal
+	RandaoPartialSig
+	// SelectionProofPartialSig is a partial signature for aggregator selection proof
+	SelectionProofPartialSig
+	// ContributionProofs is the partial selection proofs for sync committee contributions (it's an array of sigs)
+	ContributionProofs
+)
+
 type PartialSignature struct {
 	Slot          uint64
 	Signature     [96]byte `ssz-size:"96"`
@@ -14,6 +27,13 @@ type PartialSignature struct {
 	Justification *qbft.SignedMessageHeader
 }
 
-type SignedPartialSignatures struct {
+type PartialSignatures struct {
+	Type              PartialSigMsgType
 	PartialSignatures []*PartialSignature `ssz-max:"13"`
+}
+
+type SignedPartialSignatures struct {
+	PartialSignatures PartialSignatures
+	Signature         [96]byte `ssz-size:"96"`
+	Signer            uint64
 }
