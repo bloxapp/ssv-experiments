91879b9b95cbda89288f119ffd2dc605739afd25
Alon Muroch <alonmuroch@gmail.com>
2022-08-15 08:46:28 +0300

    remove PartialSignature per https://github.com/bloxapp/ssv-spec/issues/37

--- ssz_encoding/ssv/messages.go
+++ ssz_encoding/ssv/messages.go
@@ -23,7 +23,6 @@ type PartialSignature struct {
 	Slot          uint64
 	Signature     [96]byte `ssz-size:"96"`
 	SigningRoot   [32]byte `ssz-size:"32"`
-	Signer        uint64
 	Justification *qbft.SignedMessageHeader
 }
 
